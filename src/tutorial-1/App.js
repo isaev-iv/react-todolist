8612a5858c3c7d66ee6f16ddae5ae261329b0117
Liam <backgroundpositioncenter@gmail.com>
2022-06-17 14:13:21 +0300

    Переписываю полностью заново

--- src/tutorial-1/App.js
+++ src/tutorial-1/App.js
@@ -3,72 +3,72 @@ import { Paper, Divider, Button, List, Tabs, Tab } from "@mui/material";
 import { AddField } from "./components/AddField";
 import { Item } from "./components/Item";
 
-const reducer = (state, action) => {
-  if (action.type === "ADD_TASK") {
-    return [
-      ...state,
-      {
-        id: state.length !== 0 ? state[state.length - 1].id + 1 : 1,
-        text: action.payload.text,
-        completed: action.payload.checked,
-      },
-    ];
-  }
-  if (action.type === "DELETE_TASK") {
-    const newState = state.filter((item) => item.id !== action.payload);
-    return newState;
-  }
-  if (action.type === "REMOVE_ALL_TASKS") {
-    return [];
-  }
-  if (action.type === "COMPLETED_ALL_TASKS") {
-    const allCompleted = state.every((obj) => obj.completed === true);
-    return state.map((obj) => {
-      return { ...obj, completed: !allCompleted };
-    });
-  }
-  return state;
-};
+// const reducer = (state, action) => {
+//   if (action.type === "ADD_TASK") {
+//     return [
+//       ...state,
+//       {
+//         id: state.length !== 0 ? state[state.length - 1].id + 1 : 1,
+//         text: action.payload.text,
+//         completed: action.payload.checked,
+//       },
+//     ];
+//   }
+//   if (action.type === "DELETE_TASK") {
+//     const newState = state.filter((item) => item.id !== action.payload);
+//     return newState;
+//   }
+//   if (action.type === "REMOVE_ALL_TASKS") {
+//     return [];
+//   }
+//   if (action.type === "COMPLETED_ALL_TASKS") {
+//     const allCompleted = state.every((obj) => obj.completed === true);
+//     return state.map((obj) => {
+//       return { ...obj, completed: !allCompleted };
+//     });
+//   }
+//   return state;
+// };
 
 function App() {
-  const [state, dispatch] = React.useReducer(reducer, [
-    {
-      id: 1,
-      text: "Task #1",
-      completed: true,
-    },
-    {
-      id: 2,
-      text: "Task #2",
-      completed: false,
-    },
-  ]);
+  // const [state, dispatch] = React.useReducer(reducer, [
+  //   {
+  //     id: 1,
+  //     text: "Task #1",
+  //     completed: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     text: "Task #2",
+  //     completed: false,
+  //   },
+  // ]);
 
-  const addTask = (text, checked) => {
-    dispatch({
-      type: "ADD_TASK",
-      payload: {
-        text,
-        checked,
-      },
-    });
-  };
+  // const addTask = (text, checked) => {
+  //   dispatch({
+  //     type: "ADD_TASK",
+  //     payload: {
+  //       text,
+  //       checked,
+  //     },
+  //   });
+  // };
 
   const onClickRemove = () => {
-    if (window.confirm("Вы действительно хотите очистить список задач?")) {
-      dispatch({
-        type: "REMOVE_ALL_TASKS",
-      });
-    }
+    // if (window.confirm("Вы действительно хотите очистить список задач?")) {
+    //   dispatch({
+    //     type: "REMOVE_ALL_TASKS",
+    //   });
+    // }
   };
 
   const handleClickDelete = (id) => {
-    if (window.confirm("Are you really want to delete this task?")) {
-      dispatch({
-        type: "DELETE_TASK",
-        payload: id,
-      });
-    }
+    // if (window.confirm("Are you really want to delete this task?")) {
+    //   dispatch({
+    //     type: "DELETE_TASK",
+    //     payload: id,
+    //   });
+    // }
   };
 
   return (
@@ -77,7 +77,7 @@ function App() {
         <Paper className="header" elevation={0}>
           <h4>Список задач</h4>
         </Paper>
-        <AddField onAdd={addTask} />
+        <AddField />
         <Divider />
         <Tabs value={0}>
           <Tab label="Все" />
@@ -85,16 +85,7 @@ function App() {
           <Tab label="Завершённые" />
         </Tabs>
         <Divider />
-        <List>
-          {state.map((task) => (
-            <Item
-              key={task.id}
-              text={task.text}
-              completed={task.completed}
-              onDelete={() => handleClickDelete(task.id)}
-            />
-          ))}
-        </List>
+        <List></List>
         <Divider />
         <div className="check-buttons">
           <Button>Отметить всё</Button>
