723c0291d0b9d647c10ae7464692a08e972cf9e1
Liam <backgroundpositioncenter@gmail.com>
2022-06-22 14:16:14 +0300

    Переделал c использованием useReducer

--- src/tutorial-1/App.js
+++ src/tutorial-1/App.js
@@ -3,72 +3,140 @@ import { Paper, Divider, Button, List, Tabs, Tab } from "@mui/material";
 import { AddField } from "./components/AddField";
 import { Item } from "./components/Item";
 
-// const reducer = (state, action) => {
-//   if (action.type === "ADD_TASK") {
-//     return [
-//       ...state,
-//       {
-//         id: state.length !== 0 ? state[state.length - 1].id + 1 : 1,
-//         text: action.payload.text,
-//         completed: action.payload.checked,
-//       },
-//     ];
-//   }
-//   if (action.type === "DELETE_TASK") {
-//     const newState = state.filter((item) => item.id !== action.payload);
-//     return newState;
-//   }
-//   if (action.type === "REMOVE_ALL_TASKS") {
-//     return [];
-//   }
-//   if (action.type === "COMPLETED_ALL_TASKS") {
-//     const allCompleted = state.every((obj) => obj.completed === true);
-//     return state.map((obj) => {
-//       return { ...obj, completed: !allCompleted };
-//     });
-//   }
-//   return state;
-// };
+function reducer(state, action) {
+  if (action.type === "ADD_TASK") {
+    return {
+      ...state,
+      tasks: [
+        ...state.tasks,
+        {
+          id: state.tasks[state.tasks.length - 1].id + 1,
+          text: action.payload.text,
+          completed: action.payload.checked,
+        },
+      ],
+    };
+  }
+
+  if (action.type === "DELETE_TASK") {
+    return {
+      ...state,
+      tasks: state.tasks.filter((obj) => obj.id !== action.payload),
+    };
+  }
+
+  if (action.type === "TOGGLE_TASK") {
+    return {
+      ...state,
+      tasks: state.tasks.map((obj) => {
+        if (obj.id === action.payload) {
+          return {
+            ...obj,
+            completed: !obj.completed,
+          };
+        }
+        return obj;
+      }),
+    };
+  }
+
+  if (action.type === "CHECK_ALL") {
+    return {
+      ...state,
+      tasks: state.tasks.map((obj) => ({
+        ...obj,
+        completed: true,
+      })),
+    };
+  }
+
+  if (action.type === "REMOVE_ALL") {
+    return {
+      ...state,
+      tasks: [],
+    };
+  }
+  if (action.type === "SET_FILTER") {
+    return {
+      ...state,
+      filterBy: action.payload,
+    };
+  }
+}
+
+const filterIndex = {
+  all: 0,
+  completed: 1,
+  active: 2,
+};
 
 function App() {
-  // const [state, dispatch] = React.useReducer(reducer, [
-  //   {
-  //     id: 1,
-  //     text: "Task #1",
-  //     completed: true,
-  //   },
-  //   {
-  //     id: 2,
-  //     text: "Task #2",
-  //     completed: false,
-  //   },
-  // ]);
+  const [state, dispatch] = React.useReducer(reducer, {
+    filterBy: "all",
+    tasks: [
+      {
+        id: 1,
+        text: "Task #1",
+        completed: true,
+      },
+      {
+        id: 2,
+        text: "Task #12",
+        completed: true,
+      },
+      {
+        id: 3,
+        text: "Task #13",
+        completed: false,
+      },
+      {
+        id: 4,
+        text: "Task #14",
+        completed: true,
+      },
+    ],
+  });
 
-  // const addTask = (text, checked) => {
-  //   dispatch({
-  //     type: "ADD_TASK",
-  //     payload: {
-  //       text,
-  //       checked,
-  //     },
-  //   });
-  // };
-
-  const onClickRemove = () => {
-    // if (window.confirm("Вы действительно хотите очистить список задач?")) {
-    //   dispatch({
-    //     type: "REMOVE_ALL_TASKS",
-    //   });
-    // }
+  const addTask = (text, checked) => {
+    console.log(text);
+    dispatch({
+      type: "ADD_TASK",
+      payload: { text, checked },
+    });
   };
 
-  const handleClickDelete = (id) => {
-    // if (window.confirm("Are you really want to delete this task?")) {
-    //   dispatch({
-    //     type: "DELETE_TASK",
-    //     payload: id,
-    //   });
-    // }
+  const onClickTaskDelete = (id) => {
+    dispatch({
+      type: "DELETE_TASK",
+      payload: id,
+    });
+  };
+
+  const toggleComplete = (id) => {
+    dispatch({
+      type: "TOGGLE_TASK",
+      payload: id,
+    });
+  };
+
+  const onCheckedAll = () => {
+    dispatch({
+      type: "CHECK_ALL",
+    });
+  };
+
+  const onRemoveAll = () => {
+    dispatch({
+      type: "REMOVE_ALL",
+    });
+  };
+
+  const setFilter = (_, newIndex) => {
+    const status = Object.keys(filterIndex)[newIndex];
+    dispatch({
+      type: "SET_FILTER",
+      payload: status,
+    });
   };
 
   return (
@@ -77,19 +145,45 @@ function App() {
         <Paper className="header" elevation={0}>
           <h4>Список задач</h4>
         </Paper>
-        <AddField />
+        <AddField onAdd={addTask} />
         <Divider />
-        <Tabs value={0}>
+        <Tabs onChange={setFilter} value={filterIndex[state.filterBy]}>
           <Tab label="Все" />
           <Tab label="Активные" />
           <Tab label="Завершённые" />
         </Tabs>
         <Divider />
-        <List></List>
+        <List>
+          {state.tasks
+            .filter((obj) => {
+              if (state.filterBy === "all") {
+                return true;
+              }
+              if (state.filterBy === "completed") {
+                return obj.completed;
+              }
+              if (state.filterBy === "active") {
+                return !obj.completed;
+              }
+            })
+            .map((obj) => (
+              <Item
+                key={obj.id}
+                text={obj.text}
+                completed={obj.completed}
+                onClickRemove={() => onClickTaskDelete(obj.id)}
+                onToggleSwitch={() => toggleComplete(obj.id)}
+              />
+            ))}
+        </List>
         <Divider />
         <div className="check-buttons">
-          <Button>Отметить всё</Button>
-          <Button>Очистить</Button>
+          <Button disabled={!state.tasks.length} onClick={onCheckedAll}>
+            Отметить всё
+          </Button>
+          <Button disabled={!state.tasks.length} onClick={onRemoveAll}>
+            Очистить
+          </Button>
         </div>
       </Paper>
     </div>
--- src/tutorial-1/components/Item.jsx
+++ src/tutorial-1/components/Item.jsx
@@ -6,20 +6,22 @@ import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
-export const Item = () => {
+export const Item = ({ text, completed, onClickRemove, onToggleSwitch }) => {
   return (
     <ListItem>
       <div className="d-flex item">
         <Checkbox
+          checked={completed}
           icon={<RadioButtonUncheckedIcon />}
           checkedIcon={<CheckCircleIcon />}
+          onChange={onToggleSwitch}
         />
-        <Typography className="item-text">Task</Typography>
+        <Typography className="item-text">{text}</Typography>
         <div className="item-buttons d-flex">
           <IconButton>
             <EditIcon style={{ fontSize: 20 }} />
           </IconButton>
-          <IconButton>
+          <IconButton onClick={onClickRemove}>
             <DeleteOutlineIcon style={{ fontSize: 20 }} />
           </IconButton>
         </div>
